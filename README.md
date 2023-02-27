bf7acba85ef033d30828554ad5039414d78cfe43
Harsimranjit Dhaliwal <dhaliwal-h@outlook.com>
2023-02-27 01:10:04 -0500

    initial commit nextjs with tailwind and typescript setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# Harsimranjit Dhaliwal's Portfolio
---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Harsimranjit Dhaliwal's Portfolio</title>
+        <meta name="description" content="Harsimranjit Dhaliwal's Portfolio" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>
+        <h1>Harsimranjit Dhaliwal's Portfolio</h1>
+        <h2>Built with Next.js and tailwindcss.</h2>
+      </main>
+    </>
+  )
+}
